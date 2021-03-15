e1d82f8435c0a330672e991443feb473ba242e3a
Nyasha <nmawungwe@gmail.com>
2021-03-15 17:32:03 +0200

    managed to connect redux to reactApp

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,120 +1,199 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import {StyleSheet, Text, View, Button, Alert, TextInput} from 'react-native'
+import {StyleSheet, Text, View, Button, Alert, TextInput, ScrollView, SafeAreaView, KeyboardAvoidingView, Platform, TouchableWithoutFeedback, Keyboard} from 'react-native'
 import {connect} from 'react-redux'
 // import { ReactReduxContext } from 'react-redux'
 // import connect from "react-redux/es/connect/connect"
 
 
 class HomeScreen extends React.Component {
+
+    
+    state = {
+        tvNumber: '',
+        decoderNumber: '',
+        soundSystemNumber: '',
+        lightsNumber: '',
+        heatersNumber: '',
+        stovesNumber: '',
+        fridgesNumber: '',
+        kettlesNumber: '',
+        microwavesNumber: '',
+        computersNumber: '',
+        printersNumber: '',
+        modemsNumber: '',
+        electricblanketsNumber: '',
+        phonesNumber: ''
+    }
+
+    TvHandler = (tvNumber) => {this.setState({ tvNumber })}
+    DecoderHandler = (decoderNumber) => {this.setState({ decoderNumber})}
+    SoundSystemHandler = (soundSystemNumber) => {this.setState({ soundSystemNumber})}
+    LightsHandler = (lightsNumber) => {this.setState({lightsNumber })}
+    HeatersHandler = (heatersNumber) => {this.setState({ heatersNumber })}
+    StovesHandler = (stovesNumber) => {this.setState({stovesNumber})}
+    FridgesHandler = (fridgesNumber) => {this.setState({fridgesNumber})}
+    KettlesHandler = (kettlesNumber) => {this.setState({kettlesNumber})}
+    MicrowavesHandler = (microwavesNumber) => {this.setState({microwavesNumber})}
+    ComputersHandler = (computersNumber) => {this.setState({computersNumber})}
+    PrintersHandler = (printersNumber) => {this.setState({printersNumber})}
+    ModemsHandler = (modemsNumber) => {this.setState({ modemsNumber})}
+    ElectricblanketsHandler = (electricblanketsNumber) => {this.setState({electricblanketsNumber})}
+    PhonesHandler = (phonesNumber) => {this.setState({phonesNumber})}
+   
+
+
+    componentDidUpdate(){
+
+    }
+
+
     render(){
         return (
-            <View style={styles.container}>
-                <View style={styles.inputRow}>
-                    <Text>Number of TVs: </Text>
-                    <TextInput style={styles.textInput}
-                        placeholder="Number of TVs"
-                        keyboardType='number-pad'
-                    />
+            <KeyboardAvoidingView
+                behavior={Platform.OS === "ios" ? "padding" : "height"}
+                style={styles.container}
+            >
+                <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                <View 
+                    style={styles.inner}
+                >
+                    <View style={styles.inputRow}>
+                        <Text>Number of TVs: </Text>
+                        <TextInput style={styles.textInput}
+                            placeholder="Number of TVs"
+                            keyboardType='number-pad'
+                            onChangeText={this.TvHandler}
+                            value={this.state.tvNumber}
+                        />
+                    </View>
+                    <View style={styles.inputRow}>
+                        <Text>Number of Decoders: </Text>
+                        <TextInput style={styles.textInput}
+                            placeholder="Number of Decoders"
+                            keyboardType='number-pad'
+                            onChangeText={this.DecoderHandler}
+                            value={this.state.decoderNumber}
+                        />
+                    </View>
+                    <View style={styles.inputRow}>
+                        <Text>Number of Sound Systems: </Text>
+                        <TextInput style={styles.textInput}
+                            placeholder="Number of Sound Systems"
+                            keyboardType='number-pad'
+                            onChangeText={this.SoundSystemHandler}
+                            value={this.state.soundSystemNumber}
+                        />
+                    </View>
+                    <View style={styles.inputRow}>
+                        <Text>Number of Lights: </Text>
+                        <TextInput style={styles.textInput}
+                            placeholder="Number of Lights"
+                            keyboardType='number-pad'
+                            onChangeText={this.LightsHandler}
+                            value={this.state.lightsNumber}
+                        />
+                    </View>
+                    <View style={styles.inputRow}>
+                        <Text>Number of Heaters: </Text>
+                        <TextInput style={styles.textInput}
+                            placeholder="Number of Heaters"
+                            keyboardType='number-pad'
+                            onChangeText={this.HeatersHandler}
+                            value={this.state.heatersNumber}
+                        />
+                    </View>
+                    <View style={styles.inputRow}>
+                        <Text>Number of Stoves: </Text>
+                        <TextInput style={styles.textInput}
+                            placeholder="Number of Stoves"
+                            keyboardType='number-pad'
+                            onChangeText={this.StovesHandler}
+                            value={this.state.stovesNumber}
+                        />
+                    </View>
+                    <View style={styles.inputRow}>
+                        <Text>Number of Fridges: </Text>
+                        <TextInput style={styles.textInput}
+                            placeholder="Number of Fridges"
+                            keyboardType='number-pad'
+                            onChangeText={this.FridgesHandler}
+                            value={this.state.fridgesNumber}
+                        />
+                    </View>
+                    <View style={styles.inputRow}>
+                        <Text>Number of Kettles: </Text>
+                        <TextInput style={styles.textInput}
+                            placeholder="Number of Kettles"
+                            keyboardType='number-pad'
+                            onChangeText={this.KettlesHandler}
+                            value={this.state.kettlesNumber}
+                        />
+                    </View>
+                    <View style={styles.inputRow}>
+                        <Text>Number of Microwaves: </Text>
+                        <TextInput style={styles.textInput}
+                            placeholder="Number of Microwaves"
+                            keyboardType='number-pad'
+                            onChangeText={this.MicrowavesHandler}
+                            value={this.state.microwavesNumber}
+                        />
+                    </View>
+                    <View style={styles.inputRow}>
+                        <Text>Number of Computers: </Text>
+                        <TextInput style={styles.textInput}
+                            placeholder="Number of Computers"
+                            keyboardType='number-pad'
+                            onChangeText={this.ComputersHandler}
+                            value={this.state.computersNumber}
+                        />
+                    </View>
+                    <View style={styles.inputRow}>
+                        <Text>Number of Printers: </Text>
+                        <TextInput style={styles.textInput}
+                            placeholder="Number of Printers"
+                            keyboardType='number-pad'
+                            onChangeText={this.PrintersHandler}
+                            value={this.state.printersNumber}
+                        />
+                    </View>
+                    <View style={styles.inputRow}>
+                        <Text>Number of Modems: </Text>
+                        <TextInput style={styles.textInput}
+                            placeholder="Number of Modems"
+                            keyboardType='number-pad'
+                            onChangeText={this.ModemsHandler}
+                            value={this.state.modemsNumber}
+                        />
+                    </View>
+                    <View style={styles.inputRow}>
+                        <Text>Number of Electric Blankets: </Text>
+                        <TextInput style={styles.textInput}
+                            placeholder="Number of Electric Blankets"
+                            keyboardType='number-pad'
+                            onChangeText={this. ElectricblanketsHandler}
+                            value={this.state.electricblanketsNumber}
+                        />
+                    </View>
+                    <View style={styles.inputRow}>
+                        <Text>Number of Phones: </Text>
+                        <TextInput style={styles.textInput}
+                            placeholder="Number of Phones"
+                            keyboardType='number-pad'
+                            onChangeText={this.PhonesHandler}
+                            value={this.state.phonesNumber}
+                        />
+                    </View>
+                    <Text>Consumption: {this.props.consumption.consumption} kWh </Text>
+                    <Text>{this.state.tvNumber}</Text>
+                    {/* <Button
+                        title="Calculate"
+                        onPress={()=>{ Alert.alert('You pressed the calculate button')}}
+                    /> */}
+                    < StatusBar />
                 </View>
-                <View style={styles.inputRow}>
-                    <Text>Number of Decoders: </Text>
-                    <TextInput style={styles.textInput}
-                        placeholder="Number of Decoders"
-                        keyboardType='number-pad'
-                    />
-                </View>
-                <View style={styles.inputRow}>
-                    <Text>Number of Sound Systems: </Text>
-                    <TextInput style={styles.textInput}
-                        placeholder="Number of Sound Systems"
-                        keyboardType='number-pad'
-                    />
-                </View>
-                <View style={styles.inputRow}>
-                    <Text>Number of Lights: </Text>
-                    <TextInput style={styles.textInput}
-                        placeholder="Number of Lights"
-                        keyboardType='number-pad'
-                    />
-                </View>
-                <View style={styles.inputRow}>
-                    <Text>Number of Heaters: </Text>
-                    <TextInput style={styles.textInput}
-                        placeholder="Number of Heaters"
-                        keyboardType='number-pad'
-                    />
-                </View>
-                <View style={styles.inputRow}>
-                    <Text>Number of Stoves: </Text>
-                    <TextInput style={styles.textInput}
-                        placeholder="Number of Stoves"
-                        keyboardType='number-pad'
-                    />
-                </View>
-                <View style={styles.inputRow}>
-                    <Text>Number of Fridges: </Text>
-                    <TextInput style={styles.textInput}
-                        placeholder="Number of Fridges"
-                        keyboardType='number-pad'
-                    />
-                </View>
-                <View style={styles.inputRow}>
-                    <Text>Number of Kettles: </Text>
-                    <TextInput style={styles.textInput}
-                        placeholder="Number of Kettles"
-                        keyboardType='number-pad'
-                    />
-                </View>
-                <View style={styles.inputRow}>
-                    <Text>Number of Microwaves: </Text>
-                    <TextInput style={styles.textInput}
-                        placeholder="Number of Microwaves"
-                        keyboardType='number-pad'
-                    />
-                </View>
-                <View style={styles.inputRow}>
-                    <Text>Number of Computers: </Text>
-                    <TextInput style={styles.textInput}
-                        placeholder="Number of Computers"
-                        keyboardType='number-pad'
-                    />
-                </View>
-                <View style={styles.inputRow}>
-                    <Text>Number of Printers: </Text>
-                    <TextInput style={styles.textInput}
-                        placeholder="Number of Printers"
-                        keyboardType='number-pad'
-                    />
-                </View>
-                <View style={styles.inputRow}>
-                    <Text>Number of Modems: </Text>
-                    <TextInput style={styles.textInput}
-                        placeholder="Number of Modems"
-                        keyboardType='number-pad'
-                    />
-                </View>
-                <View style={styles.inputRow}>
-                    <Text>Number of Electric Blankets: </Text>
-                    <TextInput style={styles.textInput}
-                        placeholder="Number of Electric Blankets"
-                        keyboardType='number-pad'
-                    />
-                </View>
-                <View style={styles.inputRow}>
-                    <Text>Number of Phones: </Text>
-                    <TextInput style={styles.textInput}
-                        placeholder="Number of Phones"
-                        keyboardType='number-pad'
-                    />
-                </View>
-                <Text>Consumption: {this.props.consumption} kWh </Text>
-                {/* <Button
-                    title="Calculate"
-                    onPress={()=>{ Alert.alert('You pressed the calculate button')}}
-                /> */}
-                < StatusBar />
-            </View>
+                </TouchableWithoutFeedback>
+            </KeyboardAvoidingView>
     )
 
 
@@ -123,7 +202,7 @@ class HomeScreen extends React.Component {
 // Map Redux state to React component props 
 
 const mapStateToProps = (state) => {
-    const { consumption } = state.consumption.consumption
+    const { consumption } = state
     return { consumption }
 }
 
@@ -135,8 +214,16 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         backgroundColor: '#fff',
-        alignItems: 'center',
+        // alignItems: 'center',
+        // justifyContent: 'center',
+        // paddingTop: StatusBar.currentHeight
+    },
+    inner: {
+        padding: 24,
+        flex: 1,
         justifyContent: 'center',
+        paddingTop: StatusBar.currentHeight,
+        alignItems: 'center',
     },
     inputRow: {
         flexDirection: 'row',
